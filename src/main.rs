000c6700fe173d1aec678581dd9e8b260592bfe9
dragonslayerintraining <22734948+dragonslayerintraining@users.noreply.github.com>
2020-07-11 15:45:12 -0700

    Rewrote calculations with nalgebra

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ggez::{graphics, Context, ContextBuilder, GameResult};
 use ggez::event::{self,EventHandler};
 use ggez::input::mouse::MouseButton;
+use ggez::nalgebra::{Vector2, Point2};
 
 fn main() -> GameResult{
     let (mut ctx, mut event_loop) = ContextBuilder::new("bounce","dragonslayerintraining")
@@ -13,10 +14,8 @@ fn main() -> GameResult{
 
 #[derive(Clone)]
 struct Ball{
-    x: f32,
-    y: f32,
-    xdir: f32,
-    ydir: f32,
+    pos: Point2<f32>,
+    dir: Vector2<f32>,
 }
 
 struct World{
@@ -40,8 +39,7 @@ impl World{
     }
     fn simple_update(&mut self, dt: f32) {
         for ball in &mut self.balls {
-            ball.x+=ball.xdir*dt;
-            ball.y+=ball.ydir*dt;
+            ball.pos+=ball.dir*dt;
         }
     }
     fn update(&mut self, step: f32) {
@@ -50,38 +48,48 @@ impl World{
             let mut til: f32 = dt;
             let mut ev: PhysicsEvent = PhysicsEvent::NoEvent;
             for (i, ball) in self.balls.iter().enumerate() {
-                if ball.xdir < 0.0 {
-                    let t = (20.0-ball.x)/ball.xdir;
+                if ball.dir.x < 0.0 {
+                    let t = (20.0-ball.pos.x)/ball.dir.x;
                     if til > t { til=t; ev = PhysicsEvent::BallHWallCollision(i); }
                 }
-                if ball.xdir > 0.0 {
-                    let t = (500.0-20.0-ball.x)/ball.xdir;
+                if ball.dir.x > 0.0 {
+                    let t = (500.0-20.0-ball.pos.x)/ball.dir.x;
                     if til > t { til=t; ev = PhysicsEvent::BallHWallCollision(i); }
                 }
-                if ball.ydir < 0.0 {
-                    let t = (20.0-ball.y)/ball.ydir;
+                if ball.dir.y < 0.0 {
+                    let t = (20.0-ball.pos.y)/ball.dir.y;
                     if til > t { til=t; ev = PhysicsEvent::BallVWallCollision(i); }
                 }
-                if ball.ydir > 0.0 {
-                    let t = (500.0-20.0-ball.y)/ball.ydir;
+                if ball.dir.y > 0.0 {
+                    let t = (500.0-20.0-ball.pos.y)/ball.dir.y;
                     if til > t { til=t; ev = PhysicsEvent::BallVWallCollision(i); }
                 }
             }
             for (i, ball1) in self.balls.iter().enumerate() {
                 for (j, ball2) in self.balls.iter().enumerate() {
-                    let relx = ball2.x - ball1.x;        
-                    let rely = ball2.y - ball1.y;        
-                    let relxdir = ball2.xdir - ball1.xdir;        
-                    let relydir = ball2.ydir - ball1.ydir;      
-                    let a = relxdir*relxdir+relydir*relydir;
-                    let b = 2.0*(relxdir*relx+relydir*rely);
-                    let c = relx*relx+rely*rely-(20.0+20.0)*(20.0+20.0);
+                    //use frame of reference of first ball
+                    let relpos = ball2.pos - ball1.pos;
+                    let reldir = ball2.dir - ball1.dir;
+                    //solve ||reldir*t+relpos||=||R|| for t
+                    //||reldir||^2*t^2+2*reldir*relpos*t+||relpos||^2=||R||^2
+                    //Rewrite as the quadratic at^2+bt+c=0
+                    let a = reldir.dot(&reldir);
+                    let b = 2.0*reldir.dot(&relpos);
+                    let c = relpos.dot(&relpos)-(20.0+20.0)*(20.0+20.0);
+                    //Compute disriminant
                     let discr = b*b-4.0*a*c;
+                    //If no zeros, ignore
                     if discr < 0.0 {continue;}
+                    //Compute zeros
+                    //With current trajectory, balls will overlap between t=t1 and t=t2
                     let t1 = (-b-discr.sqrt())/2.0/a;
                     let t2 = (-b+discr.sqrt())/2.0/a;
+                    //Balls should never actually overlap
+                    //I don't know if rounding error can be a problem here
                     if t1 < 0.0 && t2 > 0.0 { panic!("Overlapping balls!"); }
+                    //If no overlap in the future, ignore
                     if t2 < 0.0 {continue;}
+                    //Register collision
                     if til > t1 { til=t1; ev = PhysicsEvent::BallBallCollision(i,j); }
                 }
             }
@@ -89,13 +97,11 @@ impl World{
             dt-=til;
             match ev {
                 PhysicsEvent::NoEvent => break,
-                PhysicsEvent::BallHWallCollision(i) => self.balls[i].xdir *= -1.0,
-                PhysicsEvent::BallVWallCollision(i) => self.balls[i].ydir *= -1.0,
+                PhysicsEvent::BallHWallCollision(i) => self.balls[i].dir.x *= -1.0,
+                PhysicsEvent::BallVWallCollision(i) => self.balls[i].dir.y *= -1.0,
                 PhysicsEvent::BallBallCollision(i,j) => {
-                    self.balls[i].xdir=0.0;
-                    self.balls[i].ydir=0.0;
-                    self.balls[j].xdir=0.0;
-                    self.balls[j].ydir=0.0;
+                    self.balls[i].dir=Vector2::new(0.0,0.0);
+                    self.balls[j].dir=Vector2::new(0.0,0.0);
                 }
 
             }
@@ -112,18 +118,18 @@ impl EventHandler for World{
         graphics::clear(ctx,graphics::WHITE);
         let circle = graphics::Mesh::new_circle(ctx,graphics::DrawMode::fill(),ggez::nalgebra::Point2::new(0.0,0.0),20.0,0.1,graphics::BLACK)?;
         for ball in &self.balls {
-            graphics::draw(ctx,&circle,(ggez::nalgebra::Point2::new(ball.x,ball.y),))?;
+            graphics::draw(ctx,&circle,(ball.pos,))?;
         }
         let ghost_circle = graphics::Mesh::new_circle(ctx,graphics::DrawMode::fill(),ggez::nalgebra::Point2::new(0.0,0.0),20.0,2.0,[0.0,0.0,0.0,0.5].into())?;
         if let Some(ball) = &self.ghost {
-            graphics::draw(ctx,&ghost_circle,(ggez::nalgebra::Point2::new(ball.x,ball.y),))?;
+            graphics::draw(ctx,&ghost_circle,(ball.pos,))?;
         }
         graphics::present(ctx)?;
         Ok(())
     }
     fn mouse_button_down_event(&mut self, _ctx: &mut Context,button: MouseButton, x: f32, y: f32){
         match button {
-            MouseButton::Left => self.ghost = Some(Ball{x:x,y:y,xdir:0.0,ydir:0.0}),
+            MouseButton::Left => self.ghost = Some(Ball{pos:Point2::new(x,y),dir:Vector2::new(0.0,0.0)}),
             _ => (),
         }
     }
@@ -131,20 +137,19 @@ impl EventHandler for World{
         match button {
             MouseButton::Left => {
                 if let Some(ball)= &self.ghost{
-                    let xdir : f32 = (x-ball.x)/10.0;
-                    let ydir : f32 = (y-ball.y)/10.0;
+                    let xdir : f32 = (x-ball.pos.x)/10.0;
+                    let ydir : f32 = (y-ball.pos.y)/10.0;
                     let mut overlap = false;
                     for ball2 in &self.balls {
-                        let dx = ball.x-ball2.x;
-                        let dy = ball.y-ball2.y;
-                        if dx*dx+dy*dy<(20.0+20.0)*(20.0+20.0) {
+                        let dr = ball.pos-ball2.pos;
+                        if dr.dot(&dr)<(20.0+20.0)*(20.0+20.0) {
                             overlap = true;
                         }
                     }
                     if overlap {
                         println!("There's already something there.");
                     }else{
-                        self.balls.push(Ball{x:ball.x,y:ball.y,xdir:xdir,ydir:ydir});
+                        self.balls.push(Ball{pos:ball.pos,dir:Vector2::new(xdir,ydir)});
                     }
                 }
                 self.ghost = None;
